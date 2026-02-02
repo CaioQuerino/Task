f567c5d562fb5577277d8d6a49dcf98ac0f150d8
Querino <caioquerino04@gmail.com>
2026-02-02 13:37:50 -0300

    chore: ajustar comandos Docker para rebuild limpo

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
-mvn clean compile
-
+# Pare e remova containers antigos
 docker-compose down
+docker system prune -a
 
+# Reconstrua
+docker-compose build --no-cache
+
+# Suba os serviços
 docker-compose up
