1185c2509f0f7aa6fc79aeb126a8cd6b379105a6
Querino <caioquerino04@gmail.com>
2026-02-03 04:32:07 -0300

    fix:(repository) implementar existsByEmail em ClientRepository

--- src/main/java/com/r2m/praticar/taskapplication/repositories/ClientRepository.java
+++ src/main/java/com/r2m/praticar/taskapplication/repositories/ClientRepository.java
@@ -3,9 +3,15 @@ package com.r2m.praticar.taskapplication.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.r2m.praticar.taskapplication.models.Client;
 
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+    
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Client c WHERE c.email = :email")
+    boolean existsByEmail(@Param("email") String email);
+    
     boolean findByEmail(String email);
 }
